23cc581e38011e82cac56348dd57472c7aba7207
Andrew Monks <a@monks.co>
2014-02-06 16:38:52 -0600

    readme

--- oldskoola.js
+++ oldskoola.js
@@ -47,51 +47,8 @@ function oldskoola() {
 //     head.appendChild(script);
 // }
 
-// function to change background to old paper texture
-function oldskoolaBackground() {
-	document.body.style.backgroundImage="url('https://raw2.github.com/amonks/oldskoola/master/paper.jpg')";
-}
 
-// function to change fonts to oldish looking google fonts
-function oldskoolaFonts() {
-	// js pasted from google fonts:
-		WebFontConfig = {
-			google: {
-				families: ['IM+Fell+DW+Pica:400,400italic:latin', 'Pinyon+Script::latin', 'Germania+One::latin', 'Miltonian::latin', 'Fruktur::latin']
-			}
-		};
-		(function () {
-			var wf = document.createElement('script');
-			wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-				'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-			wf.type = 'text/javascript';
-			wf.async = 'true';
-			var s = document.getElementsByTagName('script')[0];
-			s.parentNode.insertBefore(wf, s);
-		})();
-
-	// change fonts for elements
-	changeFontsTo("h1", "'Fruktur', serif")
-	changeFontsTo("body", "'Pinyon Script', cursive")
-	changeFontsTo("p", "'Pinyon Script', cursive")
-	changeFontsTo("h2", "'IM Fell DW Pica', serif")
-	changeFontsTo("h3", "'IM Fell DW Pica', serif")
-	changeFontsTo("h4", "'IM Fell DW Pica', serif")
-
-	// function to change an element's font
-	function changeFontsTo(element, font){
-		var h1Elements = document.getElementsByTagName(element);
-		for(var i = 0; i < h1Elements.length; i++) {
-			h1Elements[i].style.fontFamily = font;
-		}
-	}
-}
-
-
-
-
-
-
+// instead we'll just include the library's minified source here:
 /*
  * Pixastic - JavaScript Image Processing Library
  * Copyright (c) 2008 Jacob Seidelin, jseidelin@nihilogic.dk, http://blog.nihilogic.dk/
@@ -146,6 +103,48 @@ if(img.tagName.toLowerCase()=="canvas"){var res=Pixastic.applyAction(img,img,act
 return img;}}else if(Pixastic.Client.isIE()){if(typeof img.__pixastic_org_style!="undefined")
 img.style.cssText=img.__pixastic_org_style;}},Client:{hasCanvas:hasCanvas,hasCanvasImageData:hasCanvasImageData,hasGlobalAlpha:hasGlobalAlpha,isIE:function(){return!!document.all&&!!window.attachEvent&&!window.opera;}},Actions:{}}})();Pixastic.Actions.sepia={process:function(params){var mode=(parseInt(params.options.mode,10)||0);if(mode<0)mode=0;if(mode>1)mode=1;if(Pixastic.Client.hasCanvasImageData()){var data=Pixastic.prepareData(params);var rect=params.options.rect;var w=rect.width;var h=rect.height;var w4=w*4;var y=h;do{var offsetY=(y-1)*w4;var x=w;do{var offset=offsetY+(x-1)*4;if(mode){var d=data[offset]*0.299+data[offset+1]*0.587+data[offset+2]*0.114;var r=(d+39);var g=(d+14);var b=(d-36);}else{var or=data[offset];var og=data[offset+1];var ob=data[offset+2];var r=(or*0.393+og*0.769+ob*0.189);var g=(or*0.349+og*0.686+ob*0.168);var b=(or*0.272+og*0.534+ob*0.131);}
 if(r<0)r=0;if(r>255)r=255;if(g<0)g=0;if(g>255)g=255;if(b<0)b=0;if(b>255)b=255;data[offset]=r;data[offset+1]=g;data[offset+2]=b;}while(--x);}while(--y);return true;}},checkSupport:function(){return Pixastic.Client.hasCanvasImageData();}}
+
+
+
+// function to change background to old paper texture
+function oldskoolaBackground() {
+	document.body.style.backgroundImage="url('https://raw2.github.com/amonks/oldskoola/master/paper.jpg')";
+}
+
+// function to change fonts to oldish looking google fonts
+function oldskoolaFonts() {
+	// js pasted from google fonts:
+		WebFontConfig = {
+			google: {
+				families: ['IM+Fell+DW+Pica:400,400italic:latin', 'Pinyon+Script::latin', 'Germania+One::latin', 'Miltonian::latin', 'Fruktur::latin']
+			}
+		};
+		(function () {
+			var wf = document.createElement('script');
+			wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+				'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+			wf.type = 'text/javascript';
+			wf.async = 'true';
+			var s = document.getElementsByTagName('script')[0];
+			s.parentNode.insertBefore(wf, s);
+		})();
+
+	// change fonts for elements
+	changeFontsTo("h1", "'Fruktur', serif")
+	changeFontsTo("body", "'Pinyon Script', cursive")
+	changeFontsTo("p", "'Pinyon Script', cursive")
+	changeFontsTo("h2", "'IM Fell DW Pica', serif")
+	changeFontsTo("h3", "'IM Fell DW Pica', serif")
+	changeFontsTo("h4", "'IM Fell DW Pica', serif")
+
+	// function to change an element's font
+	function changeFontsTo(element, font){
+		var h1Elements = document.getElementsByTagName(element);
+		for(var i = 0; i < h1Elements.length; i++) {
+			h1Elements[i].style.fontFamily = font;
+		}
+	}
+}
 
 // function to convert images to sepia
 function oldskoolaImages() {
