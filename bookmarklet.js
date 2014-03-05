5a3298606c684213e3efc3cf0c3400fc459edc93
Andrew Monks <a@monks.co>
2014-03-05 13:09:36 -0600

    minify

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 javascript:
 var bodyTag = document.getElementsByTagName("body")[0];
 var script = document.createElement("script");
-script.src = "//raw.github.com/amonks/oldskoola.js/master/oldskoola.js";
+script.src = "//raw.github.com/amonks/oldskoola.js/master/oldskoola.min.js";
 bodyTag.appendChild(script);
 script.onload = script.onreadystatechange = function() {oldskoola();};
