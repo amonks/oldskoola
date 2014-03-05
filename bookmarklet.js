8c2c750f6c1cd151a738e144108b07878d552ca2
Andrew Monks <a@monks.co>
2014-03-05 10:55:33 -0600

    bookmarklet

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,9 @@
-javascript:var a=document.createElement("script");a.setAttribute("src","https://raw2.github.com/amonks/oldskoola/master/oldskoola.js");document.body.appendChild(a);
+javascript:
+var bodyTag = document.getElementsByTagName("body")[0];
+var script = document.createElement("script");
+script.src = "//raw2.github.com/amonks/oldskoola/master/oldskoola.js";
+bodyTag.appendChild(script);
+script.onload = script.onreadystatechange = function() {
+	oldskoolaBackground();
+	oldskoolaFonts();
+};
